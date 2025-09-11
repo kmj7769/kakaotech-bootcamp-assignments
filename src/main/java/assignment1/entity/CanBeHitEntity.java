7c0294333317f5178b18a28553ad620abb83bfe3
김민준 <kimminjun@gimminjun-ui-MacBookPro.local>
2025-09-11 21:17:17 +0900

    Entity 하위 CanBeHitEntity와 CanHitEntity 클래스 선언 및 기본 메서드 구현

---
+++ src/main/java/assignment1/entity/CanBeHitEntity.java
@@ -0,0 +1,27 @@
+package assignment1.entity;
+
+public class CanBeHitEntity extends Entity {
+
+    protected int healthPoint;
+    protected int defensivePower;
+
+    public CanBeHitEntity(int id, String name, int healthPoint, int defensivePower) {
+        super(id, name);
+        this.healthPoint = healthPoint;
+        this.defensivePower = defensivePower;
+    }
+
+    public int getHealthPoint() {
+        return healthPoint;
+    }
+
+    public int getDefensivePower() {
+        return defensivePower;
+    }
+
+    public boolean isAlive() {
+        return true;
+    }
+
+    public void decreaseHealthPoint() {}
+}
---
+++ src/main/java/assignment1/entity/CanHitEntity.java
@@ -0,0 +1,27 @@
+package assignment1.entity;
+
+public class CanHitEntity extends CanBeHitEntity {
+
+    protected int strength;
+    protected int attackSpeed;
+    protected final Object Lock = new Object();
+
+    public CanHitEntity(int id,
+                        String name,
+                        int healthPoint,
+                        int defensivePower,
+                        int strength,
+                        int attackSpeed) {
+        super(id, name, healthPoint, defensivePower);
+        this.strength = strength;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttackSpeed() {
+        return attackSpeed;
+    }
+}
