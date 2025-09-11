111686059445a6955e3916df33f1abb53d62c52f
kimminjun <rlaalswns7769@gmail.com>
2025-09-11 22:00:09 +0900

    ChampionEntity 클래스 선언 및 기본 메서드 구현

---
+++ src/main/java/assignment1/entity/ChampionEntity.java
@@ -0,0 +1,21 @@
+package assignment1.entity;
+
+public class ChampionEntity extends CanHitEntity {
+
+    private int gold;
+
+    public ChampionEntity(int id, String name, int healthPoint, int defensivePower, int strength, int attackSpeed, int gold) {
+        super(id, name, healthPoint, defensivePower, strength, attackSpeed);
+        this.gold = gold;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    protected void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public void buyItem(Item item) {}
+}
