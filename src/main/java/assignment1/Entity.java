0e7b35c1c6137ad1393ab48b85bd51630b48f03c
김민준 <kimminjun@gimminjun-ui-MacBookPro.local>
2025-09-11 17:33:57 +0900

    Entity 클래스 선언 및 메서드 구현

---
+++ src/main/java/assignment1/Entity.java
@@ -0,0 +1,24 @@
+package assignment1;
+
+public class Entity {
+
+    protected int id;
+    protected String name;
+
+    public Entity(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
