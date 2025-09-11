3e9c6ffac3ab0e1d4474e47355a2057c20b3cf50
kimminjun <rlaalswns7769@gmail.com>
2025-09-11 21:28:55 +0900

    MonsterEntity 클래스 선언 및 기본 메서드 구현

---
+++ src/main/java/assignment1/entity/MonsterEntity.java
@@ -0,0 +1,29 @@
+package assignment1.entity;
+
+public class MonsterEntity extends CanHitEntity {
+
+    protected int value;
+    protected Entity opponent;
+
+    public MonsterEntity(int id, String name, int healthPoint, int defensivePower, int strength, int attackSpeed, int value) {
+        super(id, name, healthPoint, defensivePower, strength, attackSpeed);
+        this.value = value;
+    }
+
+    public MonsterEntity(int id, int healthPoint, int defensivePower, int strength, int attackSpeed, int value) {
+        super(id, healthPoint, defensivePower, strength, attackSpeed);
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setOpponent(Entity opponent) {
+        this.opponent = opponent;
+    }
+
+    public void run() {}
+
+    protected void attack() {}
+}
